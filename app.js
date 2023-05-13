8b386b5e1cd65a4db5fb3edd47b84538feb7e3ae
tskinner429 <tskinner429@gmail.com>
2023-05-13 06:55:31 -0400

    submit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.post('/api/notes', (req, res) => {
+
+    let note = req.body;
+    notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+    note.id = notes.length.toString();
+    notes.push (note);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes);
+
+});
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
